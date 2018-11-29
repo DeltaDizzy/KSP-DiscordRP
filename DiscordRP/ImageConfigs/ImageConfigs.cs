51f71b6aec0eb5ae6fc6502acec7a91147bd9622
DeltaDizzy <EJBraswell@gmail.com>
2018-11-29 12:24:04 -0600

    Update ImageConfigs.cs

--- DiscordRP/ImageConfigs/ImageConfigs.cs
+++ DiscordRP/ImageConfigs/ImageConfigs.cs
@@ -1,2 +1,20 @@
 using System;
 using UnityEngine;
+
+namespace DiscordRP
+{
+    public class ImageConfigs : MonoBehaviour
+    {
+        UrlDir.UrlConfig[] rootNodes;
+        UrlDir.UrlConfig[] imageNodes;
+        
+        public void GetConfigs()
+        {
+            rootNodes = GameDatabase.Instance.root.GetConfigs("DISCORD_RP");
+            foreach(UrlDir.UrlConfig cfg in rootNodes)
+            {
+                imageNodes = GameDatabase.Instance.root.GetConfigs()
+            }
+        }
+    }
+}
